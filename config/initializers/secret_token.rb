c08174f2bbfb475218a126a8b79cdb006bd37cde
Steve Heckman <stevedoredolly@gmail.com>
2011-02-20 11:46:06 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeshopTracking::Application.config.secret_token = 'a4ede0a7bd586b9cc7d119190f59afc806d6da367b222224b8ba498c21a101007f803cbbedcaabe70b8a17da14e1f9afc7d57dc672a1811f26840a46bcc018d5'
